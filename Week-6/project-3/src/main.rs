575324a47e3bf78ad998f23f2de53fe3722dfe09
tracyjamesCOS101 <theresaoweziejames@outlook.com>
2023-11-20 08:24:29 +0100

    (My sixth commit.)

--- Week-6/project-3/src/main.rs
+++ Week-6/project-3/src/main.rs
@@ -2,33 +2,20 @@ use std::io;
 
 fn main() {
 
+    println!("MULTIPLICATION TABLE GENERATOR.");
     println!("Input in required values to begin.");
 
-    let mut minimum_value = String::new();
-    println!("Enter your minimum value: {}", minimum_value);
-    io::stdin().read_line(&mut minimum_value).expect("Invalid input. Try again, please.");
-    let mut minimum_value:i32 = minimum_value.trim().parse().expect("Your input has to be an integer.");
+    let mut number = String::new();
+    println!("Enter your maximum factor: {}", number);
+    io::stdin().read_line(&mut number).expect("Invalid input. Try again, please.");
+    let mut number:i32 = number.trim().parse().expect("Your input has to be an integer.");
 
-    let mut maximum_value = String::new();  //This is what number your factor is multiplied by before the table ends.
-    println!("Enter your maximum value: {}", maximum_value);
-    io::stdin().read_line(&mut maximum_value).expect("Invalid input. Try again, please.");
-    let mut maximum_value:i32 = maximum_value.trim().parse().expect("Your input has to be an integer.");
-
-    let mut factor = String::new();
-    println!("{}", factor);
-    io::stdin().read_line(&mut factor).expect("Invalid input. Try again, please.");
-    let mut factor:i32 = factor.trim().parse().expect("Your input has to be an integer.");
-
-    while factor >= 1 {
-        loop {
-            for multiple in minimum_value..maximum_value {
-                factor += factor;
-                println!("Multiples are: {}", factor);
-
-                if multiple > 25 {
-                    break;
-                }
-            }
+    for factor in 1..=number {
+        for integer in 1..=20 {
+            let multiple = factor * integer;
+            println!("{} x {} = {}", factor, integer, multiple);
         }
+        println!("");
     }
+    
 }
